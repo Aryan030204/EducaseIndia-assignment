7e7b911f0d2fd2699aa7a7366b6826376cf76747
aryan22csu031 <aryan22csu031@ncuindia.edu>
2025-03-12 18:48:55 +0530

    config updated

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const db = require("./db");
+const db = require("./config/db");
 const dotenv = require("dotenv");
 
 dotenv.config();
